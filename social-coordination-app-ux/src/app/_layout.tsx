2ee08321c5465bcd968bc230a3f60b73b83908f4
Alex Rosenthal <arosenthal1298@gmail.com>
2026-02-21 17:39:35 -0500

    setup

--- social-coordination-app-ux/src/app/_layout.tsx
+++ social-coordination-app-ux/src/app/_layout.tsx
@@ -3,13 +3,13 @@ import {
     DefaultTheme,
     ThemeProvider,
 } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { ClerkProvider } from '@clerk/clerk-expo';
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import { useColorScheme } from '@/src/hooks/use-color-scheme';
-import tokenCache from './utils/tokenCache';
-import React from 'react';
+import { tokenCache } from './utils/tokenCache';
+import React, { useEffect } from 'react';
 
 export const unstable_settings = {
     anchor: '(tabs)',
@@ -28,18 +28,33 @@ export default function RootLayout() {
                     'pk_test_ZHluYW1pYy1oZXJtaXQtMTguY2xlcmsuYWNjb3VudHMuZGV2JA'
                 }
             >
-                <Stack>
-                    <Stack.Screen
-                        name='(tabs)'
-                        options={{ headerShown: false }}
-                    />
-                    <Stack.Screen
-                        name='modal'
-                        options={{ presentation: 'modal', title: 'Modal' }}
-                    />
-                </Stack>
-                <StatusBar style='auto' />
+                <ClerkLoaded>
+                    <RootLayoutNav />
+                </ClerkLoaded>
             </ClerkProvider>
         </ThemeProvider>
+    );
+}
+
+function RootLayoutNav(): React.JSX.Element {
+    const { isLoaded, isSignedIn } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (isLoaded && isSignedIn) {
+            //router.navigate('/dashboard');
+        } else if (isLoaded && !isSignedIn) {
+            //router.navigate('/login-page');
+        }
+    }, []);
+
+    return (
+        <Stack>
+            <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+            <Stack.Screen
+                name='modal'
+                options={{ presentation: 'modal', title: 'Modal' }}
+            />
+        </Stack>
     );
 }
